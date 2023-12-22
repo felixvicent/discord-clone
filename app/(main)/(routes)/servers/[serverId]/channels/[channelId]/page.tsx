3c03048f8bdd1c50f3cb992f847d861bc211db52
Félix Vicente <felixvicent1306@gmail.com>
2023-12-22 11:45:55 -0300

    Chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,47 @@
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { db } from '@/lib/db';
+import { redirect } from "next/navigation";
+import { ChatHeader } from "@/components/chat/chat-header";
 
-export default function ChannelIdPage() {
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string
+  }
+}
+
+export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId
+    }
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id
+    }
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
   return (
-    <div>ChannelId Page</div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={params.serverId}
+        type="channel"
+      />
+    </div>
   )
 }
